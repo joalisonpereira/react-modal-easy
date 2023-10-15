2850043d586c6cbd794ca5c42214296552275f87
joalisonpereira <joalison.pereira@hotmail.com>
2023-10-15 05:31:34 -0300

    feat: implementing alpha version

--- lib/Modal/index.tsx
+++ lib/Modal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import styles from './Modal.module.css';
 
-interface ModalProps {
+export interface ModalProps {
   isVisible: boolean;
   children: React.ReactNode;
   onClose: () => void;
